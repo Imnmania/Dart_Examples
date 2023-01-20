196b51aa70f009a522cb75d42899ccd98a322b2b
Imnmania <niloyb@live.com>
2023-01-20 21:09:44 +0600

    asynchronous programming

--- asynchronous_programming/bin/example_12.dart
+++ asynchronous_programming/bin/example_12.dart
@@ -1,15 +1,21 @@
-import 'dart:async';
-
 ///* -----------------------
 ///* Absorbing Stream Errors
 ///* -----------------------
+
+import 'dart:async';
 
 void main(List<String> args) async {
   await for (final name in getNames().absorbErrorsUsingHandleError()) {
     print(name);
   }
   print('------------');
+
   await for (final name in getNames().absorbErrrorsUsingHandlers()) {
+    print(name);
+  }
+  print('------------');
+
+  await for (final name in getNames().absorbErrorUsingTransformer()) {
     print(name);
   }
 }
@@ -31,5 +37,29 @@ extension AbsorbErrors<T> on Stream<T> {
     return transform(StreamTransformer.fromHandlers(
       handleError: (error, stackTrace, sink) => sink.close(),
     ));
+  }
+
+  Stream<T> absorbErrorUsingTransformer() {
+    return transform(StreamErrorAbsorber());
+  }
+}
+
+/// Custom error absorb transformer
+class StreamErrorAbsorber<T> extends StreamTransformerBase<T, T> {
+  @override
+  Stream<T> bind(Stream<T> stream) {
+    final controller = StreamController<T>();
+    stream.listen(
+      (event) {
+        controller.sink.add(event);
+      },
+      onError: (_) {
+        // do nothing
+      },
+      onDone: () {
+        controller.close();
+      },
+    );
+    return controller.stream;
   }
 }
